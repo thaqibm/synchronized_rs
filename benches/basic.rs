451050125a448b81ccbfb651a01dfbceca4ba64a
Thaqib Mohammed <thaqibdamani0@gmail.com>
2023-06-30 20:14:38 -0400

    atomic bench

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,11 +1,25 @@
 use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
-use std::sync::Mutex;
+use std::sync::{Mutex, atomic::AtomicU64};
 use synchronized::util::{Counter, MAX_THREADS};
 
 struct MutexCounter {
     counter: Mutex<usize>
 }
 
+
+fn atomic_int_counter(per_thread: usize){
+    let counter = AtomicU64::new(0);
+    (0..MAX_THREADS).for_each(|_tid| {
+        std::thread::scope(|s| {
+            s.spawn(|| {
+                for _ in 0..per_thread  {
+                    counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                }
+            });
+        });
+    });
+    assert!(counter.load(std::sync::atomic::Ordering::Relaxed) == (per_thread * MAX_THREADS) as u64)
+}
 
 
 fn mutex_counter(per_thread: usize){
@@ -39,6 +53,9 @@ fn synchronized_counter(per_thread: usize){
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("counters");
+    group.sample_size(10000);
+    group.sampling_mode(criterion::SamplingMode::Flat);
+
     for i in [1000u64, 10000u64, 100000u64, 1000000u64, 
         2000000u64,
         3000000u64,
@@ -49,8 +66,13 @@ fn criterion_benchmark(c: &mut Criterion) {
         8000000u64,
         9000000u64,
         10000000u64].iter() {
+
+        group.bench_with_input(BenchmarkId::new("atomic counter", i), i, 
+            |b, i| b.iter(|| atomic_int_counter(*i as usize)));
+
         group.bench_with_input(BenchmarkId::new("mutex_counter", i), i, 
             |b, i| b.iter(|| mutex_counter(*i as usize)));
+
         group.bench_with_input(BenchmarkId::new("sync counter", i), i, 
             |b, i| b.iter(|| synchronized_counter(*i as usize)));
     }
--- benches/throughput.rs
+++ benches/throughput.rs
@@ -1,6 +1,7 @@
 use criterion::*;
-use std::sync::Mutex;
 use synchronized::util::{Counter, MAX_THREADS};
+use std::sync::{Mutex, atomic::AtomicU64};
+
 
 
 
@@ -9,6 +10,19 @@ struct MutexCounter {
 }
 
 
+fn atomic_int_counter(per_thread: usize){
+    let counter = AtomicU64::new(0);
+    (0..MAX_THREADS).for_each(|_tid| {
+        std::thread::scope(|s| {
+            s.spawn(|| {
+                for _ in 0..per_thread  {
+                    counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                }
+            });
+        });
+    });
+    assert!(counter.load(std::sync::atomic::Ordering::Relaxed) == (per_thread * MAX_THREADS) as u64)
+}
 
 fn mutex_counter(per_thread: usize){
     let counter = MutexCounter{counter: Mutex::new(0)};
@@ -40,18 +54,27 @@ fn synchronized_counter(per_thread: usize){
 
 
 fn bench(c: &mut Criterion) {
-    let bytes : [u8; 3] = [1,2,3];
 
     let mut group = c.benchmark_group("counter-throughput");
-    for i in [10000000u64].iter() {
-            group.throughput(Throughput::Elements(bytes.len() as u64));
+    group.sample_size(10000);
+    group.sampling_mode(criterion::SamplingMode::Flat);
+
+    for i in  [1000u64, 10000u64, 100000u64, 1000000u64, 
+        2000000u64, 3000000u64, 4000000u64, 5000000u64,
+        6000000u64, 7000000u64, 8000000u64, 9000000u64, 10000000u64].iter() {
+
+            group.throughput(Throughput::Elements(i.clone()));
+
+            group.bench_with_input(BenchmarkId::new("atomic_counter", i), i, 
+                |b, i| b.iter(|| atomic_int_counter(*i as usize)));
+
             group.bench_with_input(BenchmarkId::new("mutex_counter", i), i, 
                 |b, i| b.iter(|| mutex_counter(*i as usize)));
 
-            group.bench_with_input(BenchmarkId::new("sync counter", i), i, 
+            group.bench_with_input(BenchmarkId::new("sync_counter", i), i, 
                 |b, i| b.iter(|| synchronized_counter(*i as usize)));
-        }
 
+        }
     group.finish();
 }
 
